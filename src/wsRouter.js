659f3468e064a75e623248a698435e877cbd50ba
dev-jaemin <jay.kimjaemin@gmail.com>
2022-07-24 22:17:38 +0900

    turn server delete

--- src/wsRouter.js
+++ src/wsRouter.js
@@ -7,12 +7,6 @@ let socketToRoom = {};
 
 const pc_config = {
     iceServers: [
-        // {
-        //   urls: 'stun:[STUN_IP]:[PORT]',
-        //   'credentials': '[YOR CREDENTIALS]',
-        //   'username': '[USERNAME]'
-        // },
-
         {
             urls: [
                 "stun:stun.l.google.com:19302",
@@ -21,31 +15,6 @@ const pc_config = {
                 "stun:stun3.l.google.com:19302",
                 "stun:stun4.l.google.com:19302",
             ],
-        },
-        {
-            url: 'turn:numb.viagenie.ca',
-            credential: 'muazkh',
-            username: 'webrtc@live.com'
-        },
-        {
-            url: 'turn:192.158.29.39:3478?transport=udp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        },
-        {
-            url: 'turn:192.158.29.39:3478?transport=tcp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        },
-        {
-            url: 'turn:turn.bistri.com:80',
-            credential: 'homeo',
-            username: 'homeo'
-         },
-         {
-            url: 'turn:turn.anyfirewall.com:443?transport=tcp',
-            credential: 'webrtc',
-            username: 'webrtc'
         }
     ],
 };  
@@ -234,7 +203,6 @@ export default (wsServer, socket) => {
                 wsServer,       
                 data.roomID
             );
-            console.log("pcpcpc : ", pc);
             await pc.setRemoteDescription(data.sdp);
             let sdp = await pc.createAnswer({
                 offerToReceiveAudio: false, // 원래 false
